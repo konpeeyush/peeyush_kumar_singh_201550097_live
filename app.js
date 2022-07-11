bfaa46c4302ec355e7b6e4463b080aed261de55d
konpeeyush <piyushksingh2589@gmail.com>
2022-07-11 23:11:14 +0530

    Adding Live Website

---
+++ app.js
@@ -0,0 +1,11 @@
+let namej=document.getElementById('name') ;
+let rollno=document.getElementById('rollNo') ;
+let university=document.getElementById('university') ;
+let date=document.getElementById('date') ;
+
+namej.innerHTML=`Peeyush Kumar Singh` ;
+rollno.innerHTML=`201550097` ;
+university.innerHTML=`GLA University, Mathura` ;
+const today=new Date() ;
+date.innerHTML=`${today}`
+
